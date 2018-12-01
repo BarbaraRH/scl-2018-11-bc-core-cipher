b71cc82cf5dcf43512b4cf89c34878331d40a9aa
BarbaraRH <barbararamos@MacBook-Air-de-Barbara.local>
2018-12-01 19:41:14 -0300

    incluye mas diseño

--- src/index.js
+++ src/index.js
@@ -1,39 +1,18 @@
-/* Acá va tu código */
 
-document.getElementById("cifrar").addEventListener("click", function(){
+
+
+
+document.getElementById("cifrar").addEventListener("click", function(){  
     let offset = parseInt(document.getElementById("spaces").value);
-    let string = (document.getElementById("message").value).toUpperCase();
-    document.getElementById("result").innerHTML = "Mensaje cifrado: " + cipher.encode(offset, string);
+    let string = (document.getElementById("message").value).toUpperCase();  
+    document.getElementById("result").innerHTML = cipher.encode(offset, string);
 })
 
 document.getElementById("descifrar").addEventListener("click", function(){
     let offset = parseInt(document.getElementById("spaces").value);
-    let string = (document.getElementById("message").value).toUpperCase();
-    document.getElementById("result").innerHTML = "Mensaje descifrado: " + cipher.decode(offset, string);
+    let string = (document.getElementById("message").value).toUpperCase(); 
+    document.getElementById("result").innerHTML = cipher.decode(offset, string);
 })
 
 
 
-/*
-const cifrar = document.getElementById('cifrar').value;
-const descifrar = document.getElementById('descifrar').value;
-
-
-cifrar.addEventListener("Click", () => {
-    let offset = parseInt(document.getElementById("spaces").value);
-    let string = (document.getElementById("message").value).toUpperCase();
-    cifrar.innerHTML = cipher.encode(offset, string);
-})
-*/
-
-/*
-
-
-
-cifrar.addEventListener("Click", () => {
-    let offset = parseInt(document.getElementById("spaces").value);
-    let string = (document.getElementById("message").value).toUpperCase();
-    cifrar.innerHTML = cipher.encode(offset, string);
-})
-
-*/
