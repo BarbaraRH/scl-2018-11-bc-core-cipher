1b0ca6e5976c7a0aba66df09569db60d9d0ced4d
BarbaraRH <barbararamos@MacBook-Air-de-Barbara.local>
2018-12-03 22:10:53 -0300

    modif en encode y variable string ya no se lleva a upperCase

--- src/cipher.js
+++ src/cipher.js
@@ -1,19 +1,32 @@
 window.cipher = {
-    encode: (offset, string) => {
-
-        let result = "";
-
-        for (let i = 0; i < string.length; i++){
-            if (string[i] === " "){
-                result = result + " ";
-            } else {
-                result = result + (String.fromCharCode((string.charCodeAt(i) - 65 + offset) % 26 + 65));
-            }        
-        }
-        return result
+    encode: (offset, string) => {  // hay que arreglarlo para que funcione con offset negativo
+        if (typeof offset === "number") {;
+            let result = "";
+            for (let i = 0; i < string.length; i++){ 
+                let ascii = string.charCodeAt(i);  
+                if (ascii < 126){
+                    if (ascii >= 65 && ascii<= 90){
+                        result = result + (String.fromCharCode((ascii - 65 + offset) % 26 + 65));
+                    } else if (ascii >= 97 && ascii<= 122){
+                        result = result + (String.fromCharCode((ascii - 97 + offset) % 26 + 97));
+                    } else if (ascii >= 48 && ascii<= 57) {
+                        result = result + (String.fromCharCode((ascii - 48 + offset) % 10 + 48));
+                    } else {
+                        result = result + string[i]; // hay que ver como hacer lo del enter
+                    }   
+                } else {
+                    alert("no utilizar Ñ ni acentos");
+                    break;
+                }                                             
+            }
+            return result
+        } else {
+            alert ("Este valor debe ser un número, no una letra o palabra."); // no esta funcionando el alert
+            return " ";
+        }  
     },
-    
-    decode: (offset, string) => {
+
+    decode: (offset, string) => { // falta modificarlo para may, min y numeros del mismo modo que en encode
 
         let list = [];
         for (let i = 0; i < string.length; i++){
@@ -29,6 +42,29 @@ window.cipher = {
         return lettersJoined;
     }
 }
+
+
+/* Encode
+        if (offset >= 0 && typeof offset === "number"){
+
+            let result = "";
+
+            for (let i = 0; i < string.length; i++){
+                if (string[i] === " "){
+                    result = result + " ";
+                } else if (string[i] === "Ñ"){
+                    alert("no utilices la letra Ñ");
+                    return " ";
+                } else {
+                    result = result + (String.fromCharCode((string.charCodeAt(i) - 65 + offset) % 26 + 65));
+                }        
+            }
+        return result
+        } else {
+            alert ("Este valor debe ser un número mayor a 0.");
+            return " ";
+        }
+*/
 
 
 
