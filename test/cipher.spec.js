a6ff63d2f040affe495e95117e1c070b74473404
BarbaraRH <barbararamos@MacBook-Air-de-Barbara.local>
2018-12-05 12:48:20 -0300

    Modificado Encode, decode. Pasa test 86% Stmts y 83% Branch

--- test/cipher.spec.js
+++ test/cipher.spec.js
@@ -1,8 +1,59 @@
-  describe('cipher', () => {
 
-    it('debería ser un objeto', () => {
+describe('cipher', () => {
+
+  it('debería ser un objeto', () => {
     assert.equal(typeof cipher, 'object');
+  });
+
+  describe('cipher.encode', () => {
+
+    it('debería ser una función', () => {
+      assert.equal(typeof cipher.encode, 'function');
     });
+
+    it('debería retornar "HIJKLMNOPQRSTUVWXYZABCDEFG" para "ABCDEFGHIJKLMNOPQRSTUVWXYZ" con offest 33', () => {
+      assert.equal(cipher.encode(33, "ABCDEFGHIJKLMNOPQRSTUVWXYZ"), "HIJKLMNOPQRSTUVWXYZABCDEFG");
+    });
+
+    it('debería retornar "hijklmnopqrstuvwxyzabcdefg" para "abcdefghijklmnopqrstuvwxyz" con offest 33', () => {
+      assert.equal(cipher.encode(33, "abcdefghijklmnopqrstuvwxyz"), "hijklmnopqrstuvwxyzabcdefg");
+    });
+
+    it('debería retornar "3456789012" para "1234567890" con offset 12', () => {
+      assert.equal(cipher.encode(12, "1234567890"), "3456789012");
+    });
+
+  });
+
+  describe('cipher.decode', () => {
+
+    it('debería ser una función', () => {
+      assert.equal(typeof cipher.decode, 'function');
+    });
+
+    it('debería retornar "ABCDEFGHIJKLMNOPQRSTUVWXYZ" para "HIJKLMNOPQRSTUVWXYZABCDEFG" con offest 33', () => {
+      assert.equal(cipher.decode(33, "HIJKLMNOPQRSTUVWXYZABCDEFG"), "ABCDEFGHIJKLMNOPQRSTUVWXYZ");
+    });
+
+    it('debería retornar "abcdefghijklmnopqrstuvwxyz" para "hijklmnopqrstuvwxyzabcdefg" con offest 33', () => {
+      assert.equal(cipher.decode(33, "hijklmnopqrstuvwxyzabcdefg"), "abcdefghijklmnopqrstuvwxyz");
+    });
+
+    it('debería retornar "1234567890" para "3456789012" con offset 12', () => {
+      assert.equal(cipher.decode(12, "3456789012"), "1234567890");
+    });
+
+  });
+
+});
+
+
+
+/*  describe('cipher', () => {
+
+  it('debería ser un objeto', () => {
+    assert.equal(typeof cipher, 'object');
+  });
 
   describe('cipher.encode', () => {
 
@@ -29,3 +80,4 @@
 
   })
 })
+*/
