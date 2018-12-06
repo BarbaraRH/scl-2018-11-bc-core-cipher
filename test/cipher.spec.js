a48f1bc59f4b4639da464c22b99f8f19de527edb
BarbaraRH <barbararamos@MacBook-Air-de-Barbara.local>
2018-12-06 20:14:27 -0300

    correcion p de html, arreglo padding y proporciones en css y plt cambio pantallazo README

--- test/cipher.spec.js
+++ test/cipher.spec.js
@@ -66,38 +66,3 @@ describe('cipher', () => {
   });
 
 });
-
-
-
-/*  describe('cipher', () => {
-
-  it('debería ser un objeto', () => {
-    assert.equal(typeof cipher, 'object');
-  });
-
-  describe('cipher.encode', () => {
-
-    it('debería ser una función', () => {
-      assert.equal(typeof cipher.encode, 'function');
-    });
-
-    it('debería retornar "HIJKLMNOPQRSTUVWXYZABCDEFG" para "ABCDEFGHIJKLMNOPQRSTUVWXYZ" con offest 33', () => {
-      assert.equal(cipher.encode(33, "ABCDEFGHIJKLMNOPQRSTUVWXYZ"), "HIJKLMNOPQRSTUVWXYZABCDEFG");
-    });
-
-  })
-
-  describe('cipher.decode', () => {
-
-    it('debería ser una función', () => {
-      assert.equal(typeof cipher.decode, 'function');
-    });
-
-    it('debería retornar "ABCDEFGHIJKLMNOPQRSTUVWXYZ" para "HIJKLMNOPQRSTUVWXYZABCDEFG" con offest 33', () => {
-      assert.equal(cipher.encode(33, "HIJKLMNOPQRSTUVWXYZABCDEFG"), "ABCDEFGHIJKLMNOPQRSTUVWXYZ");
-    });
-
-
-  })
-})
-*/
